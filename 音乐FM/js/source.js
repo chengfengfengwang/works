d6d0c7f61932c6ce990a18d8becc3afeac9a3e2c
chengfengwang <602120734@qq.com>
2017-08-06 09:04:28 +0800

    Add files via upload

---
+++ 音乐FM/js/source.js
@@ -0,0 +1,34 @@
+var arr = [
+            {
+                song: '骄傲的少年',
+                songsrc: 'music1.mp3',
+                singer: '南征北战',
+                imgsrc:'image/1.jpg'
+            },
+            {
+                song: 'You',
+                songsrc: 'music2.mp3',
+                singer: 'Approaching Nirvana',
+                imgsrc:'image/2.png'
+            }, {
+                song: 'Canon In D (Guitar)',
+                songsrc: 'music3.mp3',
+                singer: 'The O\'Neill Brothers',
+                imgsrc:'image/3.jpg'
+            }, {
+                song: '不为谁而作的歌',
+                songsrc: 'music4.mp3',
+                singer: '林俊杰 徐佳莹',
+                imgsrc:'image/4.png'
+            }, {
+                song: 'Always With Me',
+                songsrc: 'music5.mp3',
+                singer: '《千与千寻》ED / アニメ',
+                imgsrc:'image/5.jpg'
+            }, {
+                song: '寻人启事',
+                songsrc: 'music6.mp3',
+                singer: '徐佳莹',
+                imgsrc:'image/6.jpg'
+            }
+        ]
